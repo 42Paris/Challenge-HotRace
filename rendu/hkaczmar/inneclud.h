aa83fa435b1e3299a94f224a7b201abdb23bada7
nini <nini@underflo.ws>
2021-04-07 14:21:29 +0200

    Woop Woop!

---
+++ rendu/hkaczmar/inneclud.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+// Might as well include all glibc while we're at it
+
+int	gnl(int fd, char **line);
---
+++ rendu/hkaczmar/pair.h
@@ -0,0 +1,22 @@
+#pragma once
+
+typedef struct node_s
+{
+	const char		*key, *value;
+	struct node_s	*left, *right;
+	int				disabled;		// Refer to [1]
+}	node;
+
+node	*new_node(const char *key, const char *value);
+node	*get_node(const char *key, node *root);
+void	insert(const char *key, const char *value, node **root);
+void	delete(const char *key, node *root);
+
+/*
+ *
+ * [1]: To be honest, I was too lazy to implement tree balancing, so I just
+ *		decided to never delete a node from the tree, saving cycles and
+ *		reallocations. Instead, nodes are marked "disabled" when they are
+ *		supposed to be deleted. Meh.
+ *
+ */

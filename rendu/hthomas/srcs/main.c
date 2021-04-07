b131130084bd66c4337ad123f625e12299c4de34
hugothms <hugothms@gmail.com>
2021-04-07 20:14:49 +0200

    strndup tmp_key

--- rendu/hthomas/srcs/main.c
+++ rendu/hthomas/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 15:04:19 by hthomas           #+#    #+#             */
-/*   Updated: 2021/04/07 19:49:09 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/04/07 20:13:01 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,13 +130,13 @@ int		main(int argc, char const *argv[])
 		type = type_entry(line, &length_key);
 		if (type == ENTRY)
 		{
-			set_data(&data, line, length_key);
-			if (!is_in_table(table, data->key))
+			char *tmp_key = strndup(line, length_key);
+			if (!is_in_table(table, tmp_key))
 			{
+				set_data(&data, line, length_key);
 				add_to_table(table, data);
 			}
-			else
-				free_data(data);
+			free(tmp_key);
 		}
 		else if (type == SEARCH)
 		{

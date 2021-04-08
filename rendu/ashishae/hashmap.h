fbd104a722f7d5e226dc53bffc849109641cef5f
Arseny Shishaev <ashishae@student.42.fr>
2021-04-08 13:07:18 +0100

    Revert "Improve"
    This reverts commit b28e555e1e8c49c2b8350976732a70c80e0b9453.

--- rendu/ashishae/hashmap.h
+++ rendu/ashishae/hashmap.h
@@ -6,22 +6,21 @@
 /*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 17:39:06 by ashishae          #+#    #+#             */
-/*   Updated: 2021/04/08 12:45:54 by ashishae         ###   ########.fr       */
+/*   Updated: 2021/04/08 09:33:26 by ashishae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HASHMAP_H
 # define HASHMAP_H
 
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-// # define TABLE_SIZE 1000000
 # define BASICHASH_SUBSTR 10
 #define TABLE_SIZE 1355000
-
 
 typedef struct s_hashmap_node {
 	const char *keyval;
@@ -39,9 +38,9 @@ int hn_cmp(hashmap_node *h, const char *key);
 int hn_cmp_kv(hashmap_node *h, const char *kv, size_t eqSign);
 hashmap_node *new_hnode(const char *kv, size_t eqSign);
 hashmap *new_hashmap();
-int hm_print(hashmap *h, const char *k);
+int hm_print(hashmap *h, const char *k, size_t eqSign);
 void hm_put(hashmap *h, const char *kv, size_t eqSign);
 
-void hm_remove(hashmap *h, const char *k);
+void hm_remove(hashmap *h, const char *k, size_t eqSign);
 
 #endif

77804ff603861fdfd573f846fa942cce8c6813e1
Antoine <antoine.gagniere@amadeus.com>
2021-04-08 11:21:46 +0200

    Works

--- rendu/angagnie/main.c
+++ rendu/angagnie/main.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 #ifndef HASHMAP_CAPACITY
-# define HASHMAP_CAPACITY 10000
+# define HASHMAP_CAPACITY 16384
 #endif
 
 typedef struct string
@@ -24,7 +24,7 @@ typedef struct hm_node
     size_t   hash;
 } t_hm_node;
 
-# define EMPTY_NODE (0xDEADBEEF)
+# define EMPTY_NODE (void*)(0xDEADBEEF)
 
 typedef struct hashmap
 {
@@ -62,31 +62,76 @@ void* ft_memdup(const void* src, size_t len)
     return dst;
 }
 
+bool hashmap_remove(t_hashmap* map, const t_string* key)
+{
+    size_t hash = fnv1a_32(*key);
+    t_hm_node* ptr = map->data + (hash % map->capacity);
+    t_hm_node* prev = NULL;
+
+    if (ptr->next == EMPTY_NODE)
+        return false;
+    while (ptr != NULL
+           && (ptr->hash != hash || strncmp(key->string, ptr->key.string, key->length) != 0))
+    {
+        prev = ptr;
+        ptr = ptr->next;
+    }
+    if (ptr == NULL)
+        return false;
+    free(ptr->key.string);
+    free(ptr->value.string);
+    if (prev == NULL)
+        ptr->next = EMPTY_NODE;
+    else
+        prev->next = ptr->next;
+    return true;
+}
+
 /* If key is already present, do nothing */
 bool hashmap_add(t_hashmap* map, const t_string* key, const t_string* value)
 {
     size_t hash = fnv1a_32(*key);
     t_hm_node* dest = map->data + (hash % map->capacity);
+    t_hm_node* prev = NULL;
 
     if (dest->next != EMPTY_NODE)
     {
-        while (dest->next != NULL && dest->hash != hash && strcmp(key->string, dest->key.string) != 0)
+        while (dest != NULL
+               && (dest->hash != hash || strncmp(key->string, dest->key.string, key->length) != 0))
+        {
+            prev = dest;
             dest = dest->next;
-
-//        t_hm_node* new = malloc(sizeof(t_hm_node));
-
-        new->next = dest->next;
-        dest->next = new;
-        dest = new;
+        }
+        if (dest != NULL)
+            return false;
+        dest = malloc(sizeof(t_hm_node));
+        prev->next = dest;
     }
+    dest->next = NULL;
     dest->hash = hash;
-    dest->key.string = hm_memdup();
+    dest->key.string = ft_memdup(key->string, key->length + 1);
+    dest->key.string[key->length] = '\0';
+    dest->key.length = key->length;
+    dest->value.string = ft_memdup(value->string, value->length + 1);
+    dest->value.string[value->length] = '\0';
+    dest->value.length = value->length;
+    return true;
 }
 
-bool hashmap_get(t_hashmap* map, const t_string* key)
+bool hashmap_get(const t_hashmap* map, const t_string* key, t_string* out_result)
 {
     size_t hash = fnv1a_32(*key);
-    t_hm_node
+    const t_hm_node* ptr = map->data + (hash % map->capacity);
+
+    if (ptr->next == EMPTY_NODE)
+        return false;
+    while (ptr != NULL
+           && (ptr->hash != hash || strncmp(key->string, ptr->key.string, key->length) != 0))
+        ptr = ptr->next;
+    if (ptr == NULL)
+        return false;
+    *out_result = ptr->value;
+    return true;
 }
 
 int main(void)
@@ -98,10 +143,14 @@ int main(void)
     size_t equal;
     t_string key, value;
 
-    if (!(map.data = malloc(sizeof(t_hm_node) * HASHMAP_CAPACITY)))
+    if (!(map.data = calloc(HASHMAP_CAPACITY, sizeof(t_hm_node))))
         return 1;
     map.capacity = HASHMAP_CAPACITY;
     map.size = 0;
+    for (unsigned i = 0; i < map.capacity; i++)
+        map.data[i].next = EMPTY_NODE;
+
+    printf("Node size : %lu\n", sizeof(t_hm_node));
     while ((read = getline(&(line.string), &getline_size, stdin)) >= 0)
     {
         line.length = strlen(line.string);
@@ -112,8 +161,8 @@ int main(void)
         {
             key.string = line.string + 1;
             key.length = line.length - 1;
-//            hm_remove(line.string + 1, line.length - 1);
-            printf("Remove '%.*s'\n", (int)key.length, key.string);
+            bool ret = hashmap_remove(&map, &key);
+            printf("Remove '%.*s' : %i\n", (int)key.length, key.string, ret);
         }
         else
         {
@@ -123,8 +172,10 @@ int main(void)
             if (line.string[equal] == '\0')
             {
                 printf("Look for '%.*s'\n", (int)line.length, line.string);
-//                if (hm_get(&line, &value))
-//                    printf("%*s: %*s\n", line.length, line.string, value.length, value.string);
+                if (hashmap_get(&map, &line, &value))
+                    printf("%.*s\n", (int)value.length, value.string);
+                else
+                    printf("%.*s: Not Found\n", (int)line.length, line.string);
             }
             else
             {
@@ -132,8 +183,8 @@ int main(void)
                 key.length = equal;
                 value.string = line.string + equal + 1;
                 value.length = line.length - equal - 1;
-                printf("Set '%.*s' as '%.*s'\n", (int)key.length, key.string, (int)value.length, value.string);
-                hashmap_add(&map, &key, &value);
+                bool ret = hashmap_add(&map, &key, &value);
+                printf("Set '%.*s' as '%.*s' : %i\n", (int)key.length, key.string, (int)value.length, value.string, ret);
             }
         }
 

ca3e2ab0790ebf03c64fecdd5a1be684100d1461
LesChats <abaudot@student.42.fr>
2021-04-08 10:32:39 +0200

    clean and know it compile ^^'

--- rendu/abaudot/hotRace.h
+++ rendu/abaudot/hotRace.h
@@ -6,16 +6,14 @@
 /*   By: abaudot <abaudot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 13:15:01 by abaudot           #+#    #+#             */
-/*   Updated: 2021/04/07 20:34:45 by abaudot          ###   ########.fr       */
+/*   Updated: 2021/04/08 10:29:18 by abaudot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HOTRACE_H
 # define HOTRACE_H
 
-# include "reader.h"
 # include "hashmap.h"
 # include <stdio.h>
 
-# define FILE_IN	0
 #endif
